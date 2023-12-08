14e1b4f52d0e2273df432198bcf1af603525e67e
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2023-12-08 07:47:50 +0700

    chore: add database configuration in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import {server} from "./aplication/server.js";
 
-const PORT = 3500;
+import mongoose from "mongoose";
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => {
+    console.info('mongoose connect')
+  });
 
+const PORT = 3500;
 server.listen(PORT, () => {
   console.log(`app running in port ${PORT}`);
 });
