78bac0dbfd035f6e2e0a7721e2a643332f8bb974
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2023-12-09 16:51:57 +0700

    chore: update validation.js

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,7 +1,10 @@
 import {errorResponse} from "../error/error_response.js";
 
 const validation = (schema, request) => {
-  const result = schema.validate(request);
+  const result = schema.validate(request, {
+    abortEarly: false,
+    allowUnknown: false
+  });
 
   if (result.error) {
     throw errorResponse(400, result.error.message)
