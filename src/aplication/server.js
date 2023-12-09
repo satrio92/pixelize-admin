5bce0a0ca8d317debfb1995b724c461f56329229
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2023-12-09 17:00:22 +0700

    chore: use auth route in server js

--- src/aplication/server.js
+++ src/aplication/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import {publicRoute} from "../routes/public_api.js";
 import {errorMiddleware} from "../middleware/error_middleware.js";
+import {authRoute} from "../routes/api.js";
 
 export const server = express();
 
 server.use(express.json());
 server.use(publicRoute);
+server.use(authRoute);
 server.use(errorMiddleware)
 
