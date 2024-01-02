b9c3baa67f04e75a7634cd456c53a4785fc11f5b
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2024-01-02 08:50:36 +0700

    test(auth): add logout test

--- tests/user.test.js
+++ tests/user.test.js
@@ -341,7 +341,7 @@ describe('GET /api/me', () => {
     });
   });
 
-  it('should not get a current user data because not have token', async () => {
+  it('should fail when get a current user data because not have token', async () => {
 
     const result = await supertest(server)
       .get('/api/me')
@@ -350,7 +350,7 @@ describe('GET /api/me', () => {
     expect(result.body.error).toBeDefined()
   });
 
-  it('should not get a current user data because the token is invalid or expired', async () => {
+  it('should fail when get a current user data because the token is invalid or expired', async () => {
 
     const result = await supertest(server)
       .get('/api/me')
@@ -359,4 +359,133 @@ describe('GET /api/me', () => {
     expect(result.status).toBe(401)
     expect(result.body.error).toBeDefined()
   });
+});
+
+describe('PATCH /api/user/update', () => {
+
+  beforeEach(async () => {
+    await supertest(server)
+      .post('/api/users')
+      .send(mockUser);
+  });
+
+  it('should update user data succesfully', async () => {
+    const user = await supertest(server)
+      .post('/api/login')
+      .send({
+        email: "alex.john.doe@example.com",
+        password: "Password123"
+      });
+
+    const result = await supertest(server)
+      .patch('/api/user/update')
+      .set('Authorization', `Bearer ${user.body.data.token}`)
+      .send({
+        name: "Emanuel John Doe",
+        username: "johndoe61"
+      });
+
+    expect(result.status).toBe(200)
+    expect(result.body.data).toEqual({
+      username: 'johndoe61',
+      name: 'Emanuel John Doe',
+      email: 'alex.john.doe@example.com'
+    });
+  });
+
+  it('should fail when update user data because not have token', async () => {
+
+    const result = await supertest(server)
+      .patch('/api/user/update')
+      .send({
+        name: "Emanuel John Doe",
+        username: "johndoe61"
+      });
+
+    expect(result.status).toBe(401)
+    expect(result.body.error).toBeDefined()
+  });
+
+  it('should fail when update user data because the token is invalid or expired', async () => {
+
+    const result = await supertest(server)
+      .patch('/api/user/update')
+      .set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2MybmFtZSI6Imt1a3Voc2F0IiwibmFtZSI6Imt1a3VoIHNhdHJpbyIsImVtYWlsIjoia3VrdWhAZ21haWwuY29tIiwiaWF0IjoxNzAyMTEyMzc4LCJleHAiOjE3MDIxMTU5Nzh9.MbaLJk4KNjuLDxBWbavACq-7Iq7Az-mJIipz54_4jLA`)
+      .send({
+        name: "Emanuel John Doe",
+        username: "johndoe61"
+      });
+
+    expect(result.status).toBe(401)
+    expect(result.body.error).toBeDefined()
+  });
+
+  it('should fail when update user data with a too short username', async () => {
+    const result = await supertest(server)
+      .post('/api/users')
+      .set('Accept', 'application/json')
+      .send({
+        username: 'john',
+      });
+
+    expect(result.status).toBe(400)
+    expect(result.body.error).toBeDefined()
+  });
+
+  it('should fail when update user data with a too long username', async () => {
+    const result = await supertest(server)
+      .post('/api/users')
+      .set('Accept', 'application/json')
+      .send({
+        username: 'alexanderjohndoe12345678',
+      });
+
+    expect(result.status).toBe(400)
+    expect(result.body.error).toBeDefined()
+  });
+});
+
+describe('POST /api/logout', () => {
+
+  beforeEach(async () => {
+    await supertest(server)
+      .post('/api/users')
+      .send(mockUser);
+  });
+
+  it('should logout user succesfully', async () => {
+    const user = await supertest(server)
+      .post('/api/login')
+      .send({
+        email: "alex.john.doe@example.com",
+        password: "Password123"
+      });
+
+    const result = await supertest(server)
+      .post('/api/logout')
+      .set('Authorization', `Bearer ${user.body.data.token}`)
+
+    expect(result.status).toBe(200)
+    expect(result.body.data).toEqual('OK');
+  });
+
+  it('should fail when get logout user because not have token', async () => {
+
+    const result = await supertest(server)
+      .get('/api/me')
+
+    expect(result.status).toBe(401)
+    expect(result.body.error).toBeDefined()
+  });
+
+  it('should fail when logout user because the token is invalid or expired', async () => {
+
+    const result = await supertest(server)
+      .get('/api/me')
+      .set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2MybmFtZSI6Imt1a3Voc2F0IiwibmFtZSI6Imt1a3VoIHNhdHJpbyIsImVtYWlsIjoia3VrdWhAZ21haWwuY29tIiwiaWF0IjoxNzAyMTEyMzc4LCJleHAiOjE3MDIxMTU5Nzh9.MbaLJk4KNjuLDxBWbavACq-7Iq7Az-mJIipz54_4jLA`);
+
+    expect(result.status).toBe(401)
+    expect(result.body.error).toBeDefined()
+  });
+
 });
